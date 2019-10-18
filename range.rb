e0e68425433395b4c13753527ea7c5ee7801985b
Rgardner25 <51680738+Rgardner25@users.noreply.github.com>
2019-10-18 09:53:22 -0500

    Create range.rb

---
+++ range.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require 'date'
+
+def during_this_day? start_date, end_date, date_to_check
+  while start_date < end_date
+    return true if start_date == date_to_check
+    start_date += 1
+  end
+
+  false
+end
+
+describe 'during_this_day?' do
+  before do
+    @beginning_of_day = Time.new(2017, 7, 31, 0, 0, 0)
+    @end_of_day = Time.new(2017, 7, 31, 23, 59, 59)
+  end
+
+  it 'returns true if passed a time object within a given range of time objects' do
+    in_day = Time.new(2017, 7, 31, 9, 59, 59)
+    expect(
+      during_this_day?(@beginning_of_day, @end_of_day, in_day)
+    ).to eq(true)
+  end
+
+  it 'returns false if passed a time object outside a given range of time objects' do
+    not_in_day = Time.new(2017, 9, 31, 9, 59, 59)
+    expect(
+      during_this_day?(@beginning_of_day, @end_of_day, not_in_day)
+    ).to eq(false)
+  end
+end
